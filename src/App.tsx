435627fb15ed4f9f3781805df4ba066e747b6d3a
riajul-swe <riajul.swe@gmail.com>
2025-11-24 20:01:08 +0600

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,23 @@
 function App() {
   return (
     <>
-    <h1>HELLO WORLD</h1>
+      <div className="erp-container">
+        <div className="erp-header">
+          <div className="erp-title">
+            <h1>ERP Candiate Manager</h1>
+            <p>Minimal professional single-page dashborad</p>
+          </div>
+          <div>
+            <button>Add Canidate</button>
+          </div>
+        </div>
+        <div className="dashborad">
+          <div className="card"><h3>10</h3><p>total candiate</p></div>
+          <div className="card"><h3>10</h3><p>total candiate</p></div>
+          <div className="card"><h3>10</h3><p>total candiate</p></div>
+          <div className="card"><h3>10</h3><p>total candiate</p></div>
+        </div>
+      </div>
     </>
   )
 }
