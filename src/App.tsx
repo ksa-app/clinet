9352040c1d9c08e2274b04113242c255679e1eb7
riajul-swe <riajul.swe@gmail.com>
2025-11-24 20:41:32 +0600

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import './App.css';
 function App() {
   return (
     <>
@@ -8,15 +8,52 @@ function App() {
             <h1>ERP Candiate Manager</h1>
             <p>Minimal professional single-page dashborad</p>
           </div>
-          <div>
-            <button>Add Canidate</button>
+          <div className="flex-row">
+            <button className="add-btn">Add Canidate</button>
           </div>
         </div>
-        <div className="dashborad">
+        <div className="dashboard">
           <div className="card"><h3>10</h3><p>total candiate</p></div>
           <div className="card"><h3>10</h3><p>total candiate</p></div>
           <div className="card"><h3>10</h3><p>total candiate</p></div>
           <div className="card"><h3>10</h3><p>total candiate</p></div>
+        </div>
+        <div className="filter">
+          <div className="filter-left">
+            <select>
+              <option>All Statge</option>
+            </select>
+            <select>
+              <option>All Statge</option>
+            </select>
+            <input />
+          </div>
+          <div className="search-filter">
+            <input />
+            <button>search</button>
+          </div>
+        </div>
+        <div className="table-wrap">
+          <table>
+            <thead>
+              <tr>
+                <th>h1</th>
+                <th>h1</th>
+                <th>h1</th>
+                <th>h1</th>
+                <th>h1</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>h1</td>
+                <td>h1</td>
+                <td>h1</td>
+                <td>h1</td>
+                <td>h1</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </>
